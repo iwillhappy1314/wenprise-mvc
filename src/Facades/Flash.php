1fe680e9828c8442cc9dd72bf609ffa83916aaaf
李二庆（Amos Lee） <iwillhappy1314@gmail.com>
2018-03-10 12:40:54 +0800

    Flash Facade 添加 type hint

--- src/Facades/Flash.php
+++ src/Facades/Flash.php
@@ -2,6 +2,21 @@
 
 namespace Wenprise\Facades;
 
+
+/**
+ * @method info($message, $redirectUrl=null, $sticky=false)
+ * @method success($message, $redirectUrl=null, $sticky=false)
+ * @method warning($message, $redirectUrl=null, $sticky=false)
+ * @method error($message, $redirectUrl=null, $sticky=false)
+ * @method sticky($message=true, $redirectUrl=null, $type)
+ * @method add($message, $type, $redirectUrl=null, $sticky=false)
+ * @method display($types=null, $print=true)
+ * @method hasErrors()
+ * @method hasMessages($type=null)
+ * @method formatMessage($msgDataArray, $type)
+ * @method doRedirect()
+ * @method clear($types=[])
+ */
 class Flash extends Facade {
 	/**
 	 * Return the service provider key responsible for the ajax class.
