14c5aacbe42d4e43c5b8b5c8bc1e499368ca35b5
李二庆（Amos Lee） <iwillhappy1314@gmail.com>
2018-03-12 22:20:05 +0800

    remove session start in framework

--- src/Flash/FlashServiceProvider.php
+++ src/Flash/FlashServiceProvider.php
@@ -8,13 +8,7 @@ use Wenprise\Foundation\ServiceProvider;
 class FlashServiceProvider extends ServiceProvider {
 	public function register() {
 		$this->app->bind( 'flash', function ( $container ) {
-
-			if ( ! session_id() ) {
-				session_start();
-			}
-
 			return new FlashMessages();
-
 		} );
 	}
 }
