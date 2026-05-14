14641550861ca91a1bb74c9861a6238b03b0fb37
Amos Lee <amos@wpcio.com>
2026-05-14 08:36:37 +0800

    refactor: change newRoute visibility from protected to public in Router class

--- src/Route/Router.php
+++ src/Route/Router.php
@@ -28,7 +28,7 @@ class Router extends IlluminateRouter
      * @param  mixed        $action
      * @return \Illuminate\Routing\Route
      */
-    protected function newRoute($methods, $uri, $action): Route
+    public function newRoute($methods, $uri, $action): Route
     {
         return (new Route($methods, $uri, $action))
             ->setRouter($this)
